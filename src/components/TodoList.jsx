187d0c2df62d7ed6df0bac84659186d15256fd66
Pial17 <pialrafsan17@gmail.com>
2024-12-20 18:44:48 +0600

    TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 
-import React from 'react';
-
 import tick from '../assets/tick.png';
 import not_tick from '../assets/not_tick.png';
 import delete_icon from '../assets/delete.png';
 
 
 const TodoList = ({text,id,isComplete,deleteTodo}) => {
-
-const TodoList = ({text}) => {
 
     return (
         <div className='flex items-center my-3 gap-2'>
@@ -20,10 +16,8 @@ const TodoList = ({text}) => {
 
             <img onClick={()=>deleteTodo(id)} src={delete_icon} className='size-5 cursor-pointer mt-3 '/>
 
-            <img src={delete_icon} className='size-5 cursor-pointer mt-3 '/>
-
         </div>
     );
 };
-}
+
 export default TodoList;
